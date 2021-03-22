aaf586110d37dce079eac81d8a1afd54a420ef01
minsung <hwannn1327@naver.com>
2021-03-23 00:49:30 +0900

    0323youtube

---
+++ js/youtube.js
@@ -0,0 +1,7 @@
+function thumbsUpDown() {
+    var thumbs = document.querySelectorAll('.thumbs');
+    var i = 0;
+    for(i = 0; i < thumbs.length; i++) {
+        thumbs[i].style.color = '#2E2EFE';
+    }
+}
